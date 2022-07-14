7f8202cf7acb4d5ae6fdff902324714262eb994a
lu cheng <cheng.lu3@amh-group.com>
2022-07-14 13:39:11 +0800

    init project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto pt-8">
+    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white">Collect Wallet</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
