9c9ab344ad2cb81edde832bc224dae7c080cab29
lu cheng <cheng.lu3@amh-group.com>
2022-07-18 16:49:43 +0800

    feat: interactive with contract

--- config/web3Modal.js
+++ config/web3Modal.js
@@ -6,6 +6,9 @@ const providerOptions = {
     package: WalletConnectProvider, // required
     options: {
       rpc: {
+        // rinkeby test net
+        4: 'https://rinkeby.infura.io/v3/5b08f1ec7d114231ab0e3fe82ea10def',
+        // bsc network
         56: 'https://bsc-dataseed1.binance.org/',
       },
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,93 @@
 <template>
   <div class="container mx-auto pt-8">
-    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="collectWallet">Collect Wallet</button>
-    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="getid">chainId</button>
-    <p>{{ account }}</p>
-    <p>余额 {{ balance }}</p>
+    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="collect">Collect Wallet</button>
+    <p>余额： {{ balance }}</p>
+    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="mint">Mint Nft</button>
 
-    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="send">发送eth</button>
+    <input v-model="methodName" type="text">
+    <button class="px-4 py-2 font-bold rounded-md bg-slate-500 text-white" @click="read">get Contract</button>
+    <p>结果：{{ result }}</p>
   </div>
 </template>
 
 <script>
 import web3Modal from '../config/web3Modal'
 import { ethers } from "ethers";
+import contractAbi from '../config/abi.json'
+
+const nftAddress = '0x27B4BC6343f247316Ba013280dB0BdE772BF822c'
 
 export default {
   name: 'cultureNft',
   data() {
     return {
       web3Provider: null,
-      provider: null,
+      ethersProvider: null,
       signer: null,
+      contractInstance: null,
       account: '',
-      balance: ''
+      balance: 0,
+      desiredChainId: 4,
+
+      // 合约测试
+      methodName: '',
+      result: '',
     }
+  },
+  created() {
+    console.log(contractAbi)
   },
   methods: {
     // connect wallet
-    async collectWallet() {
+    async collect() {
       try {
         this.web3Provider = await web3Modal.connect()
-        window.web3Provider = this.web3Provider
-        console.log('web3Provider', this.web3Provider.chainId)
-        this.provider = new ethers.providers.Web3Provider(this.web3Provider)
-        this.syncAccount()
-        this.getSign()
+        this.checkChainId()
+        this.setEther()
+        this.getAccountInfo()
+        this.setContract()
       } catch(err) {
         console.log(err)
       }
     },
+    checkChainId() {
+      if (this.web3Provider.chainId !== this.desiredChainId) {
+        // switch chainId
+        // alert('worong network')
+      }
+    },
+    async getAccountInfo() {
+      // const accounts = await this.web3Provider.enable()
+      const accounts = await this.ethersProvider.send('eth_requestAccounts', [])
+      if (accounts && accounts.length) {
+        this.account = accounts[0]
+        const balance = await this.ethersProvider.getBalance(accounts[0])
+        console.log('balance', balance)
+        this.balance = ethers.utils.formatEther(balance)
+      }
+    },
+    async setEther() {
+      this.ethersProvider = new ethers.providers.Web3Provider(this.web3Provider)
+      this.signer = this.ethersProvider.getSigner()
+    },
+    async setContract() {
+      this.contractInstance = new ethers.Contract(nftAddress, contractAbi, this.signer)
+    },
+    async mint() {
+      console.log('mint')
+    },
+    async read() {
+      if (this.methodName) {
+        this.result = await this.contractInstance[this.methodName]()
+      }
+    },
     async syncAccount() {
-      // check chainId
-      // if (provider.chainId !== 1) {
-      //   alert('Please link the right chainId')
-      //   return
-      // }
-
-      const account = await this.web3Provider.enable()
-      if (account.length) this.account = account[0]
-
-      this.balance = await this.provider.getBalance(account[0])
-
       this.web3Provider.on('chainChanged', (chainId) => {
         console.log('chainId', chainId)
-        this.syncAccount()
       })
       this.web3Provider.on('accountsChanged', (account) => {
         console.log('account', account)
-        this.syncAccount()
       })
-    },
-    async getid() {
-      console.log(this.web3Provider.chainId)
-    },
-    async getSign() {
-      this.signer = await this.provider.getSigner()
-    },
-    async send() {
-      const tx = this.signer.sendTransaction({
-        to: '0x8b88F7A1256316FeAC25E889A56cd346C8992b94',
-        value: ethers.utils.parseEther('0.0001')
-      })
-      
-      console.log(tx)
     },
   },
 }
